13de7ca3cca9e0619ce86de551ec3aaeb8c83b6b
Felix Stegmaier <felix.stegmaier@tu-dortmund.de>
2024-01-15 12:59:24 +0100

    WIP struct ref taint

---
+++ analysis-targets/unsafe-tests/src/struct-ref-02.rs
@@ -0,0 +1,42 @@
+#![feature(start)]
+
+struct Foo {
+    x: u32,
+    y: Box<[u32; 16]>,
+}
+
+impl Foo {
+    fn new() -> Self {
+        Foo {
+            x: 0,
+            y: Box::new([0; 16]),
+        }
+    }
+}
+
+#[no_mangle]
+fn bar(f: &mut Foo, x: u32, y0: u32) {
+    f.x = x;
+    f.y[0] = y0;
+}
+
+#[no_mangle]
+fn source() -> u32 {
+    42
+}
+
+#[no_mangle]
+fn sink(value: u32) -> u32 {
+    value
+}
+
+#[start]
+fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    let mut foo = Foo::new();
+    let x = source();
+    let y0 = source();
+    bar(&mut foo, x, y0);
+    sink(foo.x);
+    sink(foo.y[0]);
+    return 0;
+}
