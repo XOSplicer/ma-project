fbfb6c1e329389d6ebd2a566ac1453adb7d9b008
Felix Stegmaier <felix.stegmaier@tu-dortmund.de>
2024-02-26 19:23:12 +0100

    Add unsafe-drop-analysis

--- tools/unsafe-drop-analysis/unsafe-drop-analysis.cpp
+++ tools/unsafe-drop-analysis/unsafe-drop-analysis.cpp
@@ -7,7 +7,6 @@
  *  Template of `myphasartool` by Philipp Schubert and others under MIT License
  *****************************************************************************/
 
-#include "find_unsafe_rs.h"
 #include "phasar.h"
 #include "llvm/IR/DebugInfo.h"
 
@@ -16,51 +15,6 @@
 #include <sstream>
 
 using namespace psr;
-
-/// @brief Find all functions in the module that are marked as `unsafe`. Uses find_unsafe_rs.
-/// @param HA
-std::vector<llvm::Function const *> get_unsafe_functions(HelperAnalyses &HA)
-{
-  auto *find_unsafe_rs = find_unsafe_rs_new();
-  std::vector<llvm::Function const *> unsafe_functions;
-  llvm::outs() << "\nDeclared functions [safety]:";
-  for (const llvm::Function *const f : HA.getICFG().getAllFunctions())
-  {
-    if (f->getName() == "llvm.dbg.declare")
-    {
-      continue;
-    }
-    const llvm::DISubprogram *const sub = f->getSubprogram();
-    if (!sub)
-    {
-      continue;
-    }
-
-    auto path = std::filesystem::path(sub->getDirectory().str()) / sub->getFilename().str();
-    int is_unsafe = 0;
-    // FIXME: using col=1000 here is very hacky
-    int err = find_unsafe_rs_is_any_unsafe_location(
-        find_unsafe_rs, path.c_str(), sub->getLine(), 1000, &is_unsafe);
-    if (err)
-    {
-      PHASAR_LOG_LEVEL(WARNING, "Error in file " << path.string() << " (error " << err << " ): " << strerror(err));
-      continue;
-    }
-    llvm::outs()
-        << f->getName()
-        << " ('" << sub->getName() << "')"
-        << " : " << path.string() << ":"
-        << sub->getLine() << " ["
-        << (is_unsafe ? "unsafe" : "safe")
-        << "]\n";
-    if (is_unsafe)
-    {
-      unsafe_functions.push_back(f);
-    }
-  }
-  find_unsafe_rs_free(find_unsafe_rs);
-  return unsafe_functions;
-}
 
 /// @brief Convert IFDS leak map to XTaint leak map for uniform data structure
 psr::XTaint::LeakMap_t convert_leaks(std::map<const llvm::Instruction *, std::set<const llvm::Value *>> &leaks)
@@ -85,7 +39,7 @@ void print_leaks(psr::XTaint::LeakMap_t &leaks)
     llvm::outs() << "IR: " << *l.first << " -> Leaks values: \n";
     for (const auto v : l.second)
     {
-      llvm::outs() << " -> Value: " << *v << "\n";
+      llvm::outs() << " -> Value: " << *v << " Metadata: " << psr::getMetaDataID(v) << "\n";
     }
   }
   llvm::outs() << "\n";
@@ -97,14 +51,14 @@ int main(int argc, const char **argv)
 
   Logger::initializeStderrLogger(psr::SeverityLevel::INFO);
 
-  llvm::outs() << "unsafe-taint-check with find_unsafe_rs lib\n\n";
+  llvm::outs() << "unsafe-drop-analysis\n\n";
 
   if (argc < 2 || !std::filesystem::exists(argv[1]) ||
       std::filesystem::is_directory(argv[1]))
   {
-    llvm::errs() << "unsafe-taint-check \n"
-                    "A small PhASAR-based program to check the unsafe taint for rust\n\n"
-                    "Usage: unsafe-taint-check <LLVM IR file>\n";
+    llvm::errs() << "unsafe-drop-analysis \n"
+                    "A small PhASAR-based program to check for unsafe drops\n\n"
+                    "Usage: unsafe-drop-analysis <LLVM IR file>\n";
     return 1;
   }
 
@@ -119,44 +73,10 @@ int main(int argc, const char **argv)
     return 1;
   }
 
-  // HA.getICFG().print();
-  auto unsafe_functions = get_unsafe_functions(HA);
-  llvm::outs() << "\nUnsafe functions:\n";
-  for (auto f : unsafe_functions)
-  {
-    llvm::outs() << f->getName() << "\n";
-  }
-
-  PHASAR_LOG_LEVEL(INFO, "Testing IFDS taint analysis with unsafe functions as source:");
+  PHASAR_LOG_LEVEL(INFO, "Testing IFDS taint analysis with alloc as sources and dealloc as sink");
 
   TaintConfigData taint_config_data;
-  for (auto f : unsafe_functions)
-  {
-    // all sret(%Type) marked arguments are considered return values and therefore tainted
-    // fill vec<int> with int if f->args()[i].getParamStructRetType() != NULL
-    std::vector<unsigned int> sretArgs;
-    unsigned int arg_num = 0;
-    for (auto &arg : f->args())
-    {
-      if (arg.hasStructRetAttr())
-      {
-        sretArgs.push_back(arg_num);
-      }
-      arg_num++;
-    }
 
-    taint_config_data.Functions.push_back(
-        FunctionData{
-            .Name = f->getName().str(),
-            .ReturnCat = TaintCategory::Source,
-            .SourceValues = sretArgs,
-        });
-  }
-  taint_config_data.Functions.push_back(
-      FunctionData{
-          .Name = "sink",
-          .HasAllSinkParam = true,
-      });
   taint_config_data.Functions.push_back(
       FunctionData{
           .Name = "__rust_alloc",
@@ -167,6 +87,13 @@ int main(int argc, const char **argv)
           .Name = "__rust_alloc_zeroed",
           .ReturnCat = TaintCategory::Source,
       });
+  taint_config_data.Functions.push_back(
+    FunctionData{
+      .Name = "__rust_dealloc",
+      .HasAllSinkParam = true,
+    }
+  );
+
   // set all drop implementations as sinks
   for (const llvm::Function *const f : HA.getICFG().getAllFunctions())
   {
@@ -196,7 +123,7 @@ int main(int argc, const char **argv)
   PHASAR_LOG_LEVEL(INFO, "Solving IFDSTaintAnalysis taint problem");
   IFDSSolver S(ifds_taint_problem, &HA.getICFG());
   auto IFDSResults = S.solve();
-  // IFDSResults.dumpResults(HA.getICFG());
+  IFDSResults.dumpResults(HA.getICFG());
 
   auto ifds_taint_leaks = convert_leaks(ifds_taint_problem.Leaks);
   llvm::outs() << "\n"
